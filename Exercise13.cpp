1fd627723c58a3fde25d6c909fb1e9cd158e0303
shavaun1 <jshavaun@yahoo.com>
2022-04-22 17:39:51 -0400

    adding an update

--- Exercise13.cpp
+++ Exercise13.cpp
@@ -23,7 +23,7 @@ int main() {
 	cout << *p;				// print 0
 
 	int *p1 = nullptr;		//equivalent to int *p1 = 0
-	int *p2 = 0;			//directly initializes p2 from  the literal constant 0
+	int *p2p = 0;			//directly initializes p2p from  the literal constant 0
 
 	int zero = 0;
 	//pi = 0;				//	error: cannot assign an int to a pointer
@@ -45,19 +45,39 @@ int main() {
 
 		//i is an int; p is a pointer to int; r is a reference to int
 	int i = 1024, * p = &i, & r = i;
-	int* pl, p2;				//pl is a pointer to int; p2 is an int
-	int* p1, * p2;				// both p1 and p2 are pointers  to int
+	int* pl, p2p;				//pl is a pointer to int; p2p is an int
+	int* p1, * p2p;				// both p1 and p2p are pointers  to int
 
 	int* p1;					//p1 is a pointer to int
-	int* p2;					//p2 is a pointer to int
+	int* p2p;					//p2p is a pointer to int
 
 	int ival = 1024;
-	int* pi = &ival;			//pi points to an int
-	int** ppi = &pi;			//ppi points to an int.
+	int* pi2 = &ival;			//pi points to an int
+	int** ppi = &pi2;			//ppi points to an int.
 
 	int i = 42;
 	int* p;						//p is a pointer to int
 	int*& r = p;				//r is a reference to the pointer p
 	//r = &i;						//r refers to a pointer ; assigning  &i to r makes p point to i
+
+	const double pi = 3.14;			//pi is const: its value may not be changed
+	//double* ptr = &p;				//error: ptr is a plain pointer
+	const double* cptr = &pi;		//ok: cptr may point to a double that is const
+	//*cptr = 42					//error: cannot assign to *cptr
+
+	int errNumb = 0;
+	int* const curErr = &errNumb;	//curErr will always point to errNumb
+	const double pi = 3.14159;
+	const double* const pip = &pi;	//pip is a const pointer to a const object
+
+	int i = 0;
+	//int* const p1 = %i;			//we cannot change the value of pl; const is top level
+	const int ci = 42;				//we cannot change ci; const is top level
+	const int* p2 = &ci;			//we can change p2; const is low-level
+	const int* const p3 = p2;		//right most const is top level, left most is not
+	const int& r = ci;				//const in reference type is always low level
+
+	i = ci;							//ok: copying the calue of ci; top level const in ci ignored
+	p2 = p3;						//ok; pointed to type matches top level const in p3 is ignored
 
 }
